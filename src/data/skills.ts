c47d7c7fd6b09cc34ced4fc695715c93fa97ec47
HamzaHajMtir1 <hamzahajmtir2002@gmail.com>
2024-08-01 10:30:44 +0100

    Finish the skills part

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,10 +1,4 @@
-import {
-  SiExpress,
-  SiSocketdotio,
-  SiPrisma,
-  SiNextdotjs,
-  SiFlask,
-} from "react-icons/si";
+import { SiPrisma, SiNextdotjs } from "react-icons/si";
 
 import { type SkillsShowcaseProps } from "@/components/skills/skills-showcase";
 
@@ -17,19 +11,33 @@ import PythonSvg from "@/public/icons/python.svg";
 import ReactjsSvg from "@/public/icons/reactjs.svg";
 import TailwindcssSvg from "@/public/icons/tailwindcss.svg";
 import NodejsSvg from "@/public/icons/nodejs.svg";
+import SpringSvg from "@/public/icons/Spring_Boot.svg";
+import CSvg from "@/public/icons/C.svg";
+import JavaSvg from "@/public/icons/java.svg";
+import PHPSvg from "@/public/icons/PHP.svg";
+import BootstrapSvg from "@/public/icons/Bootstrap.svg";
+import AndroidSvg from "@/public/icons/android.svg";
 
 // CMS
 import WordPressSvg from "@/public/icons/Wordpress.svg";
 
+// ERP
+import OdooSVG from "@/public/icons/odoo.svg";
+
 // Database and ORMS
 import MongoDBSvg from "@/public/icons/mongodb.svg";
 import PostgressSvg from "@/public/icons/postgresql.svg";
+import MysqlSvg from "@/public/icons/mysql.svg";
 
 // Tools and Tech
 import GitSvg from "@/public/icons/git.svg";
 import DockerSvg from "@/public/icons/docker.svg";
-import AwsSvg from "@/public/icons/aws.svg";
+import AngularSvg from "@/public/icons/angular.svg";
 import PostmanSvg from "@/public/icons/postman.svg";
+import LinuxSvg from "@/public/icons/linux.svg";
+
+// Video Editing Tools
+import PremierSvg from "@/public/icons/Premiere_Pro.svg";
 
 export const SKILLS_DATA: SkillsShowcaseProps["skills"] = [
   {
@@ -52,6 +60,18 @@ export const SKILLS_DATA: SkillsShowcaseProps["skills"] = [
         icon: TypescriptSvg,
       },
       {
+        name: "Angular",
+        icon: AngularSvg,
+      },
+      {
+        name: "Bootstrap",
+        icon: BootstrapSvg,
+      },
+      {
+        name: "SpringBoot",
+        icon: SpringSvg,
+      },
+      {
         name: "Reactjs",
         icon: ReactjsSvg,
       },
@@ -64,8 +84,28 @@ export const SKILLS_DATA: SkillsShowcaseProps["skills"] = [
         icon: TailwindcssSvg,
       },
       {
+        name: "Nodejs",
+        icon: NodejsSvg,
+      },
+      {
         name: "Python",
         icon: PythonSvg,
+      },
+      {
+        name: "Java",
+        icon: JavaSvg,
+      },
+      {
+        name: "C Programming Language",
+        icon: CSvg,
+      },
+      {
+        name: "PHP",
+        icon: PHPSvg,
+      },
+      {
+        name: "Android",
+        icon: AndroidSvg,
       },
     ],
   },
@@ -82,20 +122,8 @@ export const SKILLS_DATA: SkillsShowcaseProps["skills"] = [
     sectionName: "ERP",
     skills: [
       {
-        name: "Nodejs",
-        icon: NodejsSvg,
-      },
-      {
-        name: "Express",
-        icon: SiExpress,
-      },
-      {
-        name: "Socket.io",
-        icon: SiSocketdotio,
-      },
-      {
-        name: "Flask",
-        icon: SiFlask,
+        name: "Odoo",
+        icon: OdooSVG,
       },
     ],
   },
@@ -107,12 +135,16 @@ export const SKILLS_DATA: SkillsShowcaseProps["skills"] = [
         icon: MongoDBSvg,
       },
       {
-        name: "Postgress",
+        name: "PostgreSQL",
         icon: PostgressSvg,
       },
       {
         name: "Prisma",
         icon: SiPrisma,
+      },
+      {
+        name: "MySQL",
+        icon: MysqlSvg,
       },
     ],
   },
@@ -128,12 +160,21 @@ export const SKILLS_DATA: SkillsShowcaseProps["skills"] = [
         icon: DockerSvg,
       },
       {
-        name: "AWS",
-        icon: AwsSvg,
-      },
-      {
         name: "Postman",
         icon: PostmanSvg,
+      },
+      {
+        name: "Linux",
+        icon: LinuxSvg,
+      },
+    ],
+  },
+  {
+    sectionName: "Video Editing Tools",
+    skills: [
+      {
+        name: "Adobe Premiere Pro",
+        icon: PremierSvg,
       },
     ],
   },
