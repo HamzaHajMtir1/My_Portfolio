9d1d587ca03769007edf5b91c957b690407ab263
HamzaHajMtir1 <hamzahajmtir2002@gmail.com>
2025-03-27 23:48:56 +0100

    Change the skills section

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -3,20 +3,14 @@ import { SiPrisma, SiNextdotjs } from "react-icons/si";
 import { type SkillsShowcaseProps } from "@/components/skills/skills-showcase";
 
 // Languages
-import HtmlSvg from "@/public/icons/html.svg";
-import CsssSvg from "@/public/icons/css.svg";
-import JavascriptSvg from "@/public/icons/javascript.svg";
-import TypescriptSvg from "@/public/icons/typescript.svg";
 import PythonSvg from "@/public/icons/python.svg";
 import ReactjsSvg from "@/public/icons/reactjs.svg";
 import TailwindcssSvg from "@/public/icons/tailwindcss.svg";
-import NodejsSvg from "@/public/icons/nodejs.svg";
 import SpringSvg from "@/public/icons/Spring_Boot.svg";
 import CSvg from "@/public/icons/C.svg";
 import JavaSvg from "@/public/icons/java.svg";
 import PHPSvg from "@/public/icons/PHP.svg";
 import BootstrapSvg from "@/public/icons/Bootstrap.svg";
-import AndroidSvg from "@/public/icons/android.svg";
 
 // CMS
 import WordPressSvg from "@/public/icons/Wordpress.svg";
@@ -33,7 +27,6 @@ import MysqlSvg from "@/public/icons/mysql.svg";
 import GitSvg from "@/public/icons/git.svg";
 import DockerSvg from "@/public/icons/docker.svg";
 import AngularSvg from "@/public/icons/angular.svg";
-import PostmanSvg from "@/public/icons/postman.svg";
 import LinuxSvg from "@/public/icons/linux.svg";
 import GitHubSvg from "@/public/icons/github.svg";
 
@@ -45,48 +38,28 @@ export const SKILLS_DATA: SkillsShowcaseProps["skills"] = [
     sectionName: "Programming languages, Frameworks and Libraries",
     skills: [
       {
-        name: "HTML",
-        icon: HtmlSvg,
-      },
-      {
-        name: "CSS",
-        icon: CsssSvg,
-      },
-      {
-        name: "Javascript",
-        icon: JavascriptSvg,
-      },
-      {
-        name: "Typescript",
-        icon: TypescriptSvg,
-      },
-      {
-        name: "Angular",
-        icon: AngularSvg,
-      },
-      {
-        name: "Bootstrap",
-        icon: BootstrapSvg,
-      },
-      {
-        name: "SpringBoot",
-        icon: SpringSvg,
+        name: "Nextjs",
+        icon: SiNextdotjs,
       },
       {
         name: "Reactjs",
         icon: ReactjsSvg,
       },
       {
-        name: "Nextjs",
-        icon: SiNextdotjs,
+        name: "Angular",
+        icon: AngularSvg,
+      },
+      {
+        name: "SpringBoot",
+        icon: SpringSvg,
       },
       {
         name: "Tailwindcss",
         icon: TailwindcssSvg,
       },
       {
-        name: "Nodejs",
-        icon: NodejsSvg,
+        name: "Bootstrap",
+        icon: BootstrapSvg,
       },
       {
         name: "Python",
@@ -103,10 +76,6 @@ export const SKILLS_DATA: SkillsShowcaseProps["skills"] = [
       {
         name: "PHP",
         icon: PHPSvg,
-      },
-      {
-        name: "Android",
-        icon: AndroidSvg,
       },
     ],
   },
@@ -132,20 +101,20 @@ export const SKILLS_DATA: SkillsShowcaseProps["skills"] = [
     sectionName: "Databases and ORMs",
     skills: [
       {
-        name: "MongoDB",
-        icon: MongoDBSvg,
-      },
-      {
-        name: "PostgreSQL",
-        icon: PostgressSvg,
-      },
-      {
         name: "Prisma",
         icon: SiPrisma,
       },
       {
         name: "MySQL",
         icon: MysqlSvg,
+      },
+      {
+        name: "PostgreSQL",
+        icon: PostgressSvg,
+      },
+      {
+        name: "MongoDB",
+        icon: MongoDBSvg,
       },
     ],
   },
@@ -163,10 +132,6 @@ export const SKILLS_DATA: SkillsShowcaseProps["skills"] = [
       {
         name: "Docker",
         icon: DockerSvg,
-      },
-      {
-        name: "Postman",
-        icon: PostmanSvg,
       },
       {
         name: "Linux",
